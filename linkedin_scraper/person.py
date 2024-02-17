472d4af9dfb68599599b3161bc6918deebc3204c
Yanko <yanko.santos@hotmail.com>
2024-02-17 17:49:51 -0300

    Update person.py

--- linkedin_scraper/person.py
+++ linkedin_scraper/person.py
@@ -127,6 +127,8 @@ class Person(Scraper):
             position_summary_text = position_details_list[1] if len(position_details_list) > 1 else None
             outer_positions = position_summary_details.find_element(By.XPATH,"*").find_elements(By.XPATH,"*")
 
+            print("len(outer_positions) -> ", len(outer_positions))
+            
             if len(outer_positions) == 4:
                 position_title = outer_positions[0].find_element(By.TAG_NAME,"span").text
                 company = outer_positions[1].find_element(By.TAG_NAME,"span").text
@@ -143,6 +145,11 @@ class Person(Scraper):
                     company = outer_positions[0].find_element(By.TAG_NAME,"span").text
                     work_times = outer_positions[1].find_element(By.TAG_NAME,"span").text
                     location = outer_positions[2].find_element(By.TAG_NAME,"span").text
+
+            print("position_title -> ", position_title)
+            print("company -> ", company)
+            print("work_times -> ", work_times)
+            print("location -> ", location)
 
             times = work_times.split("·")[0].strip() if work_times else ""
             duration = work_times.split("·")[1].strip() if len(work_times.split("·")) > 1 else None
